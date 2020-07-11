c4e4765292e14f9edaaa4f8709a79a3cfe640f62
sakshi <sakshijn99@gmail.com>
2020-07-12 01:56:10 +0530

    heroku

--- bank_api/bank_api/settings.py
+++ bank_api/bank_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +131,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+django_heroku.settings(locals())
